d34f7d1da66eb938f3a26fc7796490c241980a44
Christoph Hönes <christoph.hoenes@gmail.com>
2024-05-31 12:07:02 +0200

    do not throw error on failed sql execution, warn instead and leave andling to the following code

--- src/numerical_table_questions/sql_utils.py
+++ src/numerical_table_questions/sql_utils.py
@@ -4,7 +4,7 @@ from pathlib import PurePath
 from typing import Optional, Union
 
 import pandas as pd
-from pandasql import sqldf
+from pandasql import sqldf  # TODO try duckdb as drop-in replacement
 
 
 log_file_init_path = str(PurePath(__file__).parent.parent.parent / 'logging.ini')
@@ -22,10 +22,14 @@ def execute_sql(query: str, dataframe: pd.DataFrame, debug_empty_result=False,
         df = dataframe  # renaming for sqldf to find table
         try:
             query_result = sqldf(query)
-        except Exception as e:
+        except Exception as e:  # Log issue with query but do not interrupt program -> return None instead
+            logger.warn("Query execution failed with exception! "
+                        "See logging level debug for more info on query and table and level critical for the stack trace."
+                        )
+            logger.critical(e, exc_info=True)
             logger.info('query:\n', query)
             logger.info('table:\n', dataframe.head(5))
-            raise e
+            query_result = None
     if query_result is None and debug_empty_result:
         logger.debug('query:\n', query)
         logger.debug('table:\n', dataframe.head(3))
