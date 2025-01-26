a917e318aa48b29b8b23196b06a66e035057f92b
Christoph Hönes <christoph.hoenes@gmail.com>
2025-01-26 20:42:07 +0100

    fix: repair imports and argument names that changed during recent refactoring

--- run_data_preprocessing.py
+++ run_data_preprocessing.py
@@ -2,14 +2,14 @@ from dargparser import dargparse
 
 from numerical_table_questions.arguments import TrainingArgs, MiscArgs, TokenizationArgs
 from numerical_table_questions.data_loading import TableQADataModule
-from numerical_table_questions.model_utils import get_model_type_info
+from numerical_table_questions.utils.model_utils import get_model_type_info
 
 
 def main(parsed_arg_groups):
     args, misc_args, tokenizer_args = parsed_arg_groups
     dm = TableQADataModule(get_model_type_info(args.model_name_or_path),
-                           table_corpus=args.table_corpus,
-                           dataset_name=args.dataset_name,
+                           table_corpus=args.table_corpus_name,
+                           dataset_name=args.dataset_suffix,
                            train_batch_size=args.batch_size_per_device,
                            eval_batch_size=args.eval_batch_size_per_device,
                            tokenizing_args=tokenizer_args,
