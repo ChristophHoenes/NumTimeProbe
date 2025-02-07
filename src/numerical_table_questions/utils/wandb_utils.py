db5283eeb9d9c0bbda347618c79d8622985b2d9c
Christoph Hönes <christoph.hoenes@gmail.com>
2025-02-07 13:00:19 +0100

    feat(wandb_utils): add functions to get run and artifact from wandb

--- src/numerical_table_questions/utils/wandb_utils.py
+++ src/numerical_table_questions/utils/wandb_utils.py
@@ -1,4 +1,6 @@
+import wandb
 import warnings
+from pathlib import Path
 from typing import List, Union
 
 import torch
@@ -31,3 +33,25 @@ def get_wandb_logger(misc_args, tags: Union[str, List[str]] = []) -> WandbLogger
         tags=[*tags, *misc_args.wandb_tags],
         name=misc_args.wandb_run_name,
     )
+
+
+def get_artifact(run_id: str, artifact_name: str, version_tag: str = 'latest', return_local_path: bool = False) -> Union[wandb.Artifact, List[str]]:
+    api = wandb.Api()
+    if artifact_name == 'model':
+        artifact_name = f"model-{run_id}:{version_tag}"
+    else:
+        artifact_name = f"run-{run_id}-{artifact_name}:{version_tag}"
+    artifact = api.artifact(f"{WANDB_ENTITY}/{WANDB_PROJECT}/{artifact_name}")
+    if return_local_path:
+        local_path = Path.cwd() / 'artifacts' / artifact_name
+        if local_path.is_dir():
+            return [str(file) for file in local_path.iterdir()]
+        else:
+            filepath_string = artifact.download()
+            return [str(file) for file in Path(filepath_string).iterdir()]
+    return artifact
+
+
+def get_run(run_id: str):
+    api = wandb.Api()
+    return api.run(f"{WANDB_ENTITY}/{WANDB_PROJECT}/{run_id}")
