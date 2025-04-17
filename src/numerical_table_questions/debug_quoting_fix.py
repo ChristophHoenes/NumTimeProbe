03617938b1c9a11db3c312501b12903f041dfb4a
Christoph Hönes <christoph.hoenes@gmail.com>
2025-04-17 10:27:51 +0200

    move debug scripts in unstaged debug folder

--- src/numerical_table_questions/debug_quoting_fix.py
+++
@@ -1,9 +0,0 @@
-import datasets
-from numerical_table_questions.data_utils import consistent_quoting_map
-from numerical_table_questions.data_utils import extract_column_names_from_table
-
-
-dat = datasets.Dataset.load_from_disk('/home/mamba/.cache/gittables_group_filtered_standard_templates_test_quality_20k/250128_1032_17_123456')
-dat_tab = datasets.Dataset.load_from_disk('/home/mamba/.cache/gittables_group_filtered_test_tables/241002_1315_37_395441')
-table_column_map = extract_column_names_from_table(dat_tab)
-dat_quote = dat.map(consistent_quoting_map, fn_kwargs={'table_column_names_map': table_column_map}, num_proc=1, load_from_cache_file=False)
--- src/numerical_table_questions/debug_statistics.py
+++
@@ -1,8 +0,0 @@
-import datasets
-from numerical_table_questions.data_utils import gather_statistics_map, aggregate_statistics_map
-
-dat = datasets.Dataset.load_from_disk('/home/mamba/.cache/gittables_group_filtered_standard_templates_test_quality_20k_quotes/250201_1539_11_123456')
-
-stats_only = dat.map(gather_statistics_map, remove_columns=dat.column_names, num_proc=1)
-
-stats_only_agg = aggregate_statistics_map(stats_only)
